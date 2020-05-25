1cb746ce5788db9e509595b0511016cbf9bedb52
MaciejGL <58422989+MaciejGL@users.noreply.github.com>
2020-05-25 11:59:41 +0200

    Update Portfolio display
    Fixed from plain html to more flexible js with array of objects

--- js/main.js
+++ js/main.js
@@ -1,159 +1,176 @@
-const fontBurger = document.querySelector('.fa-bars');
-const fontX = document.querySelector('.fa-times');
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('nav');
+import { projects } from "./portfolio.js";
 
+const fontBurger = document.querySelector(".fa-bars");
+const fontX = document.querySelector(".fa-times");
+const burger = document.querySelector(".burger");
+const nav = document.querySelector("nav");
 
+const createPortfolio = () => {
+  const portfolioHook = document.getElementById("projects");
+  for (let i = 0; i < projects.length; i++) {
+    const div = document.createElement("div");
+    div.className = "project";
+    div.id = `projectPort${i + 1}`;
+    div.setAttribute("data-all", "all");
+    div.setAttribute("data-status", projects[i].status);
 
-burger.addEventListener('click', function () {
+    const pictureLinked = document.createElement("a");
+    pictureLinked.setAttribute("href", projects[i].live);
+    pictureLinked.setAttribute("target", "_blank");
 
-    fontBurger.classList.toggle('show');
-    fontX.classList.toggle('show');
-    nav.classList.toggle('show');
-    burger.classList.toggle('show');
+    const img = document.createElement("img");
+    img.src = projects[i].imgSrc;
+    img.setAttribute("alt", projects[i].imgAlt);
 
+    const title = document.createElement("h3");
+    title.textContent = projects[i].title;
+
+    const p = document.createElement("p");
+    p.textContent = projects[i].description;
+
+    const btn = document.createElement("div");
+    btn.className = "buttons";
+    btn.innerHTML = `<a href="${projects[i].live}" class="button" target="_blank">Live</a><a href="${projects[i].code}" class="button" target="_blank">Code</a>
+      `;
+    pictureLinked.appendChild(img);
+    div.appendChild(pictureLinked);
+    div.appendChild(title);
+    div.appendChild(p);
+    div.appendChild(btn);
+    portfolioHook.appendChild(div);
+  }
+};
+createPortfolio();
+
+burger.addEventListener("click", function () {
+  fontBurger.classList.toggle("show");
+  fontX.classList.toggle("show");
+  nav.classList.toggle("show");
+  burger.classList.toggle("show");
 });
 
+document.addEventListener("click", (e) => {
+  if (e.target.parentNode !== burger && nav.classList.contains("show")) {
+    fontBurger.classList.toggle("show");
+    fontX.classList.toggle("show");
+    nav.classList.toggle("show");
+    burger.classList.toggle("show");
+  }
+});
 
-document.addEventListener('click', (e) => {
-    if (e.target.parentNode !== burger && nav.classList.contains('show')) {
-        fontBurger.classList.toggle('show');
-        fontX.classList.toggle('show');
-        nav.classList.toggle('show');
-        burger.classList.toggle('show');
-
-
-    }
-})
-
-
-
-
-// Skills 
-const skillsSection = document.getElementById('skills');
-const liSkills = document.querySelectorAll('.skills li');
-const skills = document.querySelectorAll('.skillContainer');
-
+// Skills
+const skillsSection = document.getElementById("skills");
+const liSkills = document.querySelectorAll(".skills li");
+const skills = document.querySelectorAll(".skillContainer");
 
 const displayCorrectList = (e, buttons, list, show = null, hide = null) => {
-    buttons.forEach(btn => {
-        const btnId = e.target.id
-        if (btnId == btn.id) {
-
-
-
-            btn.classList.add('checked')
-            list.forEach(skill => {
-                if (btnId == skill.dataset.all || btnId == skill.dataset.status) {
-                    skill.classList.add(hide);
-                    setTimeout(() => {
-                        skill.classList.remove(hide);
-                        skill.classList.add(show);
-                        skill.style.display = 'flex'
-                    }, 300)
-                } else {
-                    skill.classList.remove(show)
-                    skill.classList.add(hide)
-                    setTimeout(() => {
-                        skill.style.display = 'none'
-                    }, 300)
-                }
-            })
+  buttons.forEach((btn) => {
+    const btnId = e.target.id;
+    if (btnId == btn.id) {
+      btn.classList.add("checked");
+      list.forEach((skill) => {
+        if (btnId == skill.dataset.all || btnId == skill.dataset.status) {
+          skill.classList.add(hide);
+          setTimeout(() => {
+            skill.classList.remove(hide);
+            skill.classList.add(show);
+            skill.style.display = "flex";
+          }, 300);
         } else {
-            btn.classList.remove('checked')
+          skill.classList.remove(show);
+          skill.classList.add(hide);
+          setTimeout(() => {
+            skill.style.display = "none";
+          }, 300);
         }
-    })
-
-
-}
-skillsSection.addEventListener('click', (e) => {
-    if (e.target.tagName == "LI") {
-        displayCorrectList(e, liSkills, skills, 'visible', 'unvisible');
+      });
+    } else {
+      btn.classList.remove("checked");
     }
-})
+  });
+};
+skillsSection.addEventListener("click", (e) => {
+  if (e.target.tagName == "LI") {
+    displayCorrectList(e, liSkills, skills, "visible", "unvisible");
+  }
+});
 
 const addDisplay = () => {
-    skills.forEach(skill => {
-        if (skill.getAttribute('data-status') == 'learned') {
-            skill.style.display = 'flex'
-        } else {
-            skill.style.display = 'none'
-        }
-    })
-    projectsList.forEach(skill => {
-        if (skill.classList.contains('project')) {
-            skill.style.display = 'flex'
-        } else {
-            skill.style.display = 'none'
-        }
-    })
-}
-
-
+  skills.forEach((skill) => {
+    if (skill.getAttribute("data-status") == "learned") {
+      skill.style.display = "flex";
+    } else {
+      skill.style.display = "none";
+    }
+  });
+  projectsList.forEach((skill) => {
+    if (skill.classList.contains("project")) {
+      skill.style.display = "flex";
+    } else {
+      skill.style.display = "none";
+    }
+  });
+};
 
 // Lates Project
 
-const projectContainer = document.querySelector('.projectImages');
+const projectContainer = document.querySelector(".projectImages");
 
-projectContainer.addEventListener('click', () => {
-    projectContainer.classList.toggle('clicked')
-})
+projectContainer.addEventListener("click", () => {
+  projectContainer.classList.toggle("clicked");
+});
 
 // Portfolio Section
-const portfolioSection = document.querySelector('.portfolio');
-const btnPortfolio = document.querySelectorAll('.portfolio li');
-const projectsList = document.querySelectorAll('.project');
+const portfolioSection = document.querySelector(".portfolio");
+const btnPortfolio = document.querySelectorAll(".portfolio li");
+const projectsList = document.querySelectorAll(".project");
 
-portfolioSection.addEventListener('click', (e) => {
-    if (e.target.tagName == "LI") {
-        if (e.target.id == 'all') {
-            projectsList.forEach(project => project.setAttribute('data-all', 'all'))
-        } else if (e.target.id != 'all') {
-            projectsList.forEach(project => project.removeAttribute("all"))
-        }
-        displayCorrectList(e, btnPortfolio, projectsList, 'visible', 'unvisible')
+portfolioSection.addEventListener("click", (e) => {
+  if (e.target.tagName == "LI") {
+    if (e.target.id == "all") {
+      projectsList.forEach((project) =>
+        project.setAttribute("data-all", "all")
+      );
+    } else if (e.target.id != "all") {
+      projectsList.forEach((project) => project.removeAttribute("all"));
     }
-})
-
-
-
-
+    displayCorrectList(e, btnPortfolio, projectsList, "visible", "unvisible");
+  }
+});
 
 // Appear on scroll
 const appearOnScroll = (item) => {
-    const windowHeight = window.innerHeight;
-    const windowPosition = window.pageYOffset;
-    const itemPosition = item.offsetTop;
-    if (windowPosition >= itemPosition - windowHeight / 1.35) {
-        item.style.transform = 'translate(0, 0)';
-        item.style.opacity = '1'
-    }
-}
+  const windowHeight = window.innerHeight;
+  const windowPosition = window.pageYOffset;
+  const itemPosition = item.offsetTop;
+  if (windowPosition >= itemPosition - windowHeight / 1.35) {
+    item.style.transform = "translate(0, 0)";
+    item.style.opacity = "1";
+  }
+};
 
-window.addEventListener('scroll', () => {
-    // skills
-    appearOnScroll(document.getElementById('skill1'))
-    appearOnScroll(document.getElementById('skill2'))
-    appearOnScroll(document.getElementById('skill3'))
-    appearOnScroll(document.getElementById('skill4'))
-    // Latest Project
-    appearOnScroll(document.getElementById('project1'))
-    appearOnScroll(document.getElementById('project2'))
-    // abilities
-    appearOnScroll(document.getElementById('abilitie1'))
-    appearOnScroll(document.getElementById('abilitie2'))
-    appearOnScroll(document.getElementById('abilitie3'))
-    // Portfolio
+window.addEventListener("scroll", () => {
+  // skills
+  appearOnScroll(document.getElementById("skill1"));
+  appearOnScroll(document.getElementById("skill2"));
+  appearOnScroll(document.getElementById("skill3"));
+  appearOnScroll(document.getElementById("skill4"));
+  // Latest Project
+  appearOnScroll(document.getElementById("project1"));
+  appearOnScroll(document.getElementById("project2"));
+  // abilities
+  appearOnScroll(document.getElementById("abilitie1"));
+  appearOnScroll(document.getElementById("abilitie2"));
+  appearOnScroll(document.getElementById("abilitie3"));
+  // Portfolio
 
-    appearOnScroll(document.getElementById('projectPort1'))
-    appearOnScroll(document.getElementById('projectPort2'))
-    appearOnScroll(document.getElementById('projectPort3'))
-    appearOnScroll(document.getElementById('projectPort4'))
-    appearOnScroll(document.getElementById('projectPort5'))
-    appearOnScroll(document.getElementById('projectPort6'))
-    appearOnScroll(document.getElementById('projectPort7'))
-
-})
-
+  appearOnScroll(document.getElementById("projectPort1"));
+  appearOnScroll(document.getElementById("projectPort2"));
+  appearOnScroll(document.getElementById("projectPort3"));
+  appearOnScroll(document.getElementById("projectPort4"));
+  appearOnScroll(document.getElementById("projectPort5"));
+  appearOnScroll(document.getElementById("projectPort6"));
+  appearOnScroll(document.getElementById("projectPort7"));
+});
 
 addDisplay();
---
+++ js/portfolio.js
@@ -0,0 +1,74 @@
+const projects = [
+  {
+    status: "commercial",
+    imgSrc: "images/portfolio/mgportfolio.jpeg",
+    imgAlt: "Maciej Glowacki - Portfolio",
+    title: "My personal portfolio",
+    description: `My own project from scratch. After 6 months of learning I decided to create my own site to
+      possibly promote my skills. Technologies used: HTML CSS/SASS JS`,
+    live: "https://maciejgl.github.io",
+    code: "https://github.com/MaciejGL/maciejgl.github.io",
+  },
+  {
+    status: "course",
+    imgSrc: "images/portfolio/inviteSomeoneApp.jpg",
+    imgAlt: "InviteSomeone App TreeHouse",
+    title: "InviteSomeone - App",
+    description: `Application to create a list of people we want to invite. In this you can assign if the
+    person has responded to come, update a name or remove the person. All data is stored
+    dynamicly on localStorage of users browser.`,
+    live: "https://maciejgl.github.io/Invitation-List-App/",
+    code: "https://github.com/MaciejGL/Invitation-List-App",
+  },
+  {
+    status: "course",
+    imgSrc: "images/portfolio/employees.jpeg",
+    imgAlt: "Employees Directory with search method - API",
+    title: "Employees Directory - API",
+    description: `mple app using API to get random Employees list plus JavaScript to filter in all provided
+    data of employees.`,
+    live: "https://maciejgl.github.io/Project-08-API-Employee-Directory",
+    code: "https://github.com/MaciejGL/Project-08-API-Employee-Directory",
+  },
+  {
+    status: "course",
+    imgSrc: "images/portfolio/dashboard.jpeg",
+    imgAlt: "Dashboard App with charts plugins",
+    title: "WebApp Dashboard",
+    description: `Responsive Dashboard - I used for it charts plugins and localStorage to set and get settings.`,
+    live: "https://maciejgl.github.io/Project-07-WebApp-Dashboard",
+    code: "https://github.com/MaciejGL/Project-07-WebApp-Dashboard",
+  },
+  {
+    status: "course",
+    imgSrc: "images/portfolio/wheelofsucces.jpeg",
+    imgAlt: "Game Wheel of success",
+    title: "Game - Wheel of success",
+    description: `Simple game using OOP and modules and DOM manipulation. This project was a great challange,
+    but it helped to undestand better how to build a logic.`,
+    live: "https://maciejgl.github.io/Project-06-Wheel-of-Success",
+    code: "https://github.com/MaciejGL/Project-06-Wheel-of-Success",
+  },
+  {
+    status: "course",
+    imgSrc: "images/portfolio/portfolio01.jpeg",
+    imgAlt: "First attend to website project",
+    title: "TreeHouse project 1",
+    description: `TreeHouse first project. Simply HTML and CSS. It was a very first project to intruduce how to
+    build a website.`,
+    live: "https://maciejgl.github.io/Project-01-Portfolio",
+    code: "https://github.com/MaciejGL/Project-01-Portfolio",
+  },
+  {
+    status: "course",
+    imgSrc: "images/portfolio/responsivegallery1.jpeg",
+    imgAlt: "Responsive Gallery with lightbox",
+    title: "Responsive Gallery",
+    description: `Basic way of adapting plugins like LightBox and adjusting search filter to display desired
+    pictures.`,
+    live: "https://maciejgl.github.io/Project-05-Responsive-Gallery",
+    code: "https://github.com/MaciejGL/Project-05-Responsive-Gallery",
+  },
+];
+
+export { projects };
