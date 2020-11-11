ae29da62d5ed729e58f33e46f4377c62d5eb7308
Maciej Glowacki <m.glowacki01@gmail.com>
2020-11-11 15:55:55 +0100

    Portfolio v1.1

--- js/portfolio.js
+++ js/portfolio.js
@@ -18,66 +18,34 @@ const projects = [
     live: "https://mystifying-meninsky-eb1cba.netlify.app/",
     code: "https://github.com/MaciejGL/todoreact/",
   },
-  // {
-  //   status: "course",
-  //   imgSrc: "images/portfolio/inviteSomeoneApp.jpg",
-  //   imgAlt: "InviteSomeone App TreeHouse",
-  //   title: "InviteSomeone - App",
-  //   description: `Application to create a list of people we want to invite. In this you can assign if the
-  //   person has responded to come, update a name or remove the person. All data is stored
-  //   dynamicly on localStorage of users browser.`,
-  //   live: "https://maciejgl.github.io/Invitation-List-App/",
-  //   code: "https://github.com/MaciejGL/Invitation-List-App",
-  // },
-  // {
-  //   status: "course",
-  //   imgSrc: "images/portfolio/employees.jpeg",
-  //   imgAlt: "Employees Directory with search method - API",
-  //   title: "Employees Directory - API",
-  //   description: `mple app using API to get random Employees list plus JavaScript to filter in all provided
-  //   data of employees.`,
-  //   live: "https://maciejgl.github.io/Project-08-API-Employee-Directory",
-  //   code: "https://github.com/MaciejGL/Project-08-API-Employee-Directory",
-  // },
-  // {
-  //   status: "course",
-  //   imgSrc: "images/portfolio/dashboard.jpeg",
-  //   imgAlt: "Dashboard App with charts plugins",
-  //   title: "WebApp Dashboard",
-  //   description: `Responsive Dashboard - I used for it charts plugins and localStorage to set and get settings.`,
-  //   live: "https://maciejgl.github.io/Project-07-WebApp-Dashboard",
-  //   code: "https://github.com/MaciejGL/Project-07-WebApp-Dashboard",
-  // },
-  // {
-  //   status: "course",
-  //   imgSrc: "images/portfolio/wheelofsucces.jpeg",
-  //   imgAlt: "Game Wheel of success",
-  //   title: "Game - Wheel of success",
-  //   description: `Simple game using OOP and modules and DOM manipulation. This project was a great challange,
-  //   but it helped to undestand better how to build a logic.`,
-  //   live: "https://maciejgl.github.io/Project-06-Wheel-of-Success",
-  //   code: "https://github.com/MaciejGL/Project-06-Wheel-of-Success",
-  // },
-  // {
-  //   status: "course",
-  //   imgSrc: "images/portfolio/portfolio01.jpeg",
-  //   imgAlt: "First attend to website project",
-  //   title: "TreeHouse project 1",
-  //   description: `TreeHouse first project. Simply HTML and CSS. It was a very first project to intruduce how to
-  //   build a website.`,
-  //   live: "https://maciejgl.github.io/Project-01-Portfolio",
-  //   code: "https://github.com/MaciejGL/Project-01-Portfolio",
-  // },
-  // {
-  //   status: "course",
-  //   imgSrc: "images/portfolio/responsivegallery1.jpeg",
-  //   imgAlt: "Responsive Gallery with lightbox",
-  //   title: "Responsive Gallery",
-  //   description: `Basic way of adapting plugins like LightBox and adjusting search filter to display desired
-  //   pictures.`,
-  //   live: "https://maciejgl.github.io/Project-05-Responsive-Gallery",
-  //   code: "https://github.com/MaciejGL/Project-05-Responsive-Gallery",
-  // },
+  {
+    status: "commercial",
+    imgSrc: "images/portfolio/todoreact.png",
+    imgAlt: "Todo Server with DB",
+    title: "Todo Server with DB",
+    description: `Todo App built on with Express and GraphQL. Data is stored on MongoDB. Stack: Node Express GraphQl Mongoose MongoDB`,
+    // live: "https://mystifying-meninsky-eb1cba.netlify.app/",
+    code: "https://github.com/MaciejGL/todo-server/",
+  },
+  {
+    status: "commercial",
+    imgSrc: "images/portfolio/workout.png",
+    imgAlt: "Workout Planner Frontend",
+    title: "Workout Planner Frontend",
+    description: `Hackaton Project Built in 1 day - Workout Planner Frontend built with React.`,
+    live: "https://vigorous-blackwell-47abc3.netlify.app/",
+    code: "https://github.com/MaciejGL/workout_planner_frontend/",
+  },
+  {
+    status: "commercial",
+    imgSrc: "images/portfolio/workout.png",
+    imgAlt: "Workout Planner Backend with DB",
+    title: "Workout Planner Backend with DB ",
+    description: `Hackaton Project Built in 1 day - REST API server with connection to MongoDB. Stack: Node, Express, Mongoose.`,
+    live: "https://vigorous-blackwell-47abc3.netlify.app/",
+    code: "https://github.com/MaciejGL/workout_planner_frontend/",
+  },
+  
 ];
 
 export { projects };
